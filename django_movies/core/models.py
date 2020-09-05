6cbf717b736df46e643b2f34161e11e3d463cff7
Agnes Gruszecka <agnieszka.gruszecka@gmail.com>
2020-09-05 13:53:30 +0200

    stworzona klasa modelu, doddano plik gitignore

---
+++ django_movies/core/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
+
+class Movie(models.Model):  #  to jest podstawowy model models.Model
+    title = models.CharField(max_length=100)
+    rating = models.IntegerField(null=True, validators=[MaxValueValidator(10), MinValueValidator(1)])
+    released = models.DateField()
+    description = models.TextField(null=True, blank=True)   # null = True, czy zazwalasz danemu rekordowi być zerowym, blank odnosie się do formularzy
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):  # metofa reprezentuje w ten sposób nowe filmy
+        return f"{self.title} from {self.released}"
+
